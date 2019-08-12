43e11f0e77d72860064eace46d2a6d81d2d5ac5e
Héctor Sebastián Trejo Luna <32781378+hstrejoluna@users.noreply.github.com>
2019-08-12 13:07:53 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
+
+function App() {
+  // state
+
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+  const [preguntaPresupuesto, guardarPreguntaPresupuesto] = useState(true);
+  const [crearGasto, guardarCrearGasto] = useState(false);
+  const [gasto, guardarGasto] = useState(false);
+  const [gastos, guardarGastos] = useState([]);
+
+  useEffect(() => {
+    if (crearGasto) {
+      const listadoGastos = [...gastos, gasto];
+      guardarGastos(listadoGastos);
+
+      // restar el presupuesto
+      const presupuestoRestante = restante - gasto.cantidadGasto;
+      guardarRestante(presupuestoRestante);
+
+      // una vez que se agrega, lo ponemos como false
+      guardarCrearGasto(false);
+    }
+  }, [crearGasto, gastos, gasto, restante]);
+
+  return (
+    <div className="App container">
+      <header>
+        <h1>Gasto Semanal</h1>
+        <div className="contenido-principal contenido">
+          {preguntaPresupuesto ? (
+            <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarPreguntaPresupuesto={guardarPreguntaPresupuesto}
+              guardarRestante={guardarRestante}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
+                />
+              </div>
+
+              <div className="one-half column">
+                <Listado gastos={gastos} />
+
+                <ControlPresupuesto
+                  presupuesto={presupuesto}
+                  restante={restante}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
