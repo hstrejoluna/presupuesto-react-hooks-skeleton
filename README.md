d0e99d16b793553b03d54053f4f07b8684acc5f3
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-24 19:39:32 -0500

    control

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # presupuesto-react-hooks-skeleton
 Control de presupuesto semanal
+
+Utilizo Skeleton Framework para los elementos.
+
+Deploy de demostración:
+https://goofy-mccarthy-70ee2a.netlify.com/
--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -5,7 +5,7 @@ const ControlPresupuesto = ({ presupuesto, restante }) => (
   <Fragment>
     <div className="alert alert-primary">Presupuesto: $ {presupuesto}</div>
     <div className={revisarPresupuesto(presupuesto, restante)}>
-      Restante : $ {restante}
+      Restantes : $ {restante}
     </div>
   </Fragment>
 );
